87c707477951b67d5171981ad1de2d12cce10929
fitrialfiana <70354770+fitrialfiana@users.noreply.github.com>
2022-11-14 22:14:33 +0700

    Update 1 - Tampilan halaman
    Tampilan foto belum teratur

---
+++ IDCARD/src/test/java/ws/b/IDCARD/IdcardApplicationTests.java
@@ -0,0 +1,13 @@
+package ws.b.IDCARD;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IdcardApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
